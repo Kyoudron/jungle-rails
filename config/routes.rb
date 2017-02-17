e5f17a7d08fd86277e5cd8acc58d313a59794ec0
Kapish <kapish.k.1@gmail.com>
2017-02-17 13:33:04 -0500

    added POST create /step 7

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@
 
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :reviews, only: [:create] #add delete?
+  end
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
