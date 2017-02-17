4303e020f212d874f151d85c6d549a3ebddc50d4
Kapish <kapish.k.1@gmail.com>
2017-02-17 16:30:58 -0500

    added and fixed css, bootstrap

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
-  before_filer :authorize
+  before_filter :authorize
 
   def show
   end
