17885f163abe2169806b1ce54e3823b810658f7a
Kapish <kapish.k.1@gmail.com>
2017-02-19 12:42:45 -0500

    added dashboard redirection

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,27 @@
 class Admin::DashboardController < ApplicationController
+
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
-  before_filter :authorize
-
-  def show
+  def index
+    @category = Category.all
   end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
